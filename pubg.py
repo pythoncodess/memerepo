22b47512d7bcb26f58a653e045b40069e8131405
pythoncodess <45357572+pythoncodess@users.noreply.github.com>
2018-12-20 07:58:04 +0000

    Create pubg.py

---
+++ pubg.py
@@ -0,0 +1,204 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import asyncio
+import colorsys
+import random
+import platform
+from discord import Game, Embed, Color, Status, ChannelType
+import os
+import functools
+import time
+import datetime
+import requests
+import json
+import aiohttp
+
+client = commands.Bot(description="Here is some command for you", command_prefix=commands.when_mentioned_or("mk!"), pm_help = False)
+
+@client.event
+async def on_ready():
+	print('Logged in as '+client.user.name+'')
+	print('--------')
+	print('--------')
+	print('Started Meme king') #add_your_bot_name_here
+	return await client.change_presence(game=discord.Game(name='Meme king | mk!help')) #add_your_bot_status_here
+
+@client.command(pass_context=True)
+async def tweet(ctx, usernamename:str, *, txt:str):
+    url = f"https://nekobot.xyz/api/imagegen?type=tweet&username={usernamename}&text={txt}"
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get(url) as r:
+            res = await r.json()
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+            embed.set_image(url=res['message'])
+            embed.title = "{} twitted: {}".format(usernamename, txt)
+            await client.say(embed=embed)
+
+@client.command(pass_context=True)
+async def lovedetect(ctx, user: discord.Member = None, *, user2: discord.Member = None):
+    shipuser1 = user.name
+    shipuser2 = user2.name
+    useravatar1 = user.avatar_url
+    useravatar2s = user2.avatar_url
+    self_length = len(user.name)
+    first_length = round(self_length / 2)
+    first_half = user.name[0:first_length]
+    usr_length = len(user2.name)
+    second_length = round(usr_length / 2)
+    second_half = user2.name[second_length:]
+    finalName = first_half + second_half
+    score = random.randint(0, 100)
+    filled_progbar = round(score / 100 * 10)
+    counter_ = 'â–ˆ' * filled_progbar + 'â€ â€' * (10 - filled_progbar)
+    url = f"https://nekobot.xyz/api/imagegen?type=ship&user1={useravatar1}&user2={useravatar2s}"
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get(url) as r:
+            res = await r.json()
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(title=f"{shipuser1} â¤ {shipuser2} Love each others", description=f"Love\n`{counter_}` Score:**{score}% **\nLoveName:**{finalName}**", color = discord.Color((r << 16) + (g << 8) + b))
+            embed.set_image(url=res['message'])
+            await client.say(embed=embed)
+
+
+
+@client.command(pass_context = True)
+async def ping(ctx):
+    channel = ctx.message.channel
+    t1 = time.perf_counter()
+    await client.send_typing(channel)
+    t2 = time.perf_counter()
+    await client.say("Ping: {}ms".format(round((t2-t1)*1000)))
+
+
+@client.event
+async def on_member_join(member):
+    for channel in member.server.channels:
+        if channel.name == 'â˜…å½¡-welcome-å½¡â˜…':
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='Do not forget to check rules and never try to break any one of them', color = discord.Color((r << 16) + (g << 8) + b))
+            embed.add_field(name='__Thanks for joining__', value='**Hope you will be active here.**', inline=True)
+            embed.add_field(name='Your join position is', value=member.joined_at)
+            embed.set_thumbnail(url=member.avatar_url)
+            await client.send_message(channel, embed=embed)
+
+		
+
+@client.event
+async def on_member_join(member):
+    for channel in member.server.channels:
+        if channel.name == 'ðŸŽ‰-welcome-ðŸŽŠ':
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='Do not forget to check rules and never try to break any one of them', color = discord.Color((r << 16) + (g << 8) + b))
+            embed.add_field(name='__Thanks for joining__', value='**Hope you will be active here.**', inline=True)
+            embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/486489391083159574/520207004334292992/Loading.gif') 
+            embed.set_image(url = member.avatar_url)
+            embed.add_field(name='__Join position__', value='{}'.format(str(member.server.member_count)), inline=True)
+            embed.add_field(name='Time of joining', value=member.joined_at)
+            await client.send_message(channel, embed=embed)
+
+	
+@client.event
+async def on_member_remove(member):
+    for channel in member.server.channels:
+        if channel.name == 'ðŸŽ‰-welcome-ðŸŽŠ':
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(title=f'{member.name} just left {member.server.name}', description='Bye bye ðŸ‘‹! We will miss you ðŸ˜¢', color = discord.Color((r << 16) + (g << 8) + b))
+            embed.add_field(name='__User left__', value='**Hope you will be back soon ðŸ˜•.**', inline=True)
+            embed.add_field(name='Your join position was', value=member.joined_at)
+            embed.set_thumbnail(url=member.avatar_url)
+            await client.send_message(channel, embed=embed)
+
+
+@client.command(pass_context = True)
+@commands.has_permissions(administrator=True)
+async def setupwelcome(ctx):
+    if ctx.message.author.bot:
+      return
+    else:
+      server = ctx.message.server
+      everyone_perms = discord.PermissionOverwrite(send_messages=False, read_messages=True)
+      everyone = discord.ChannelPermissions(target=server.default_role, overwrite=everyone_perms)
+      await client.create_channel(server, 'ðŸŽ‰-welcome-ðŸŽŠ',everyone)
+	
+		
+		
+@client.event
+async def on_member_join(member):
+    print("In our server" + member.name + " just joined")
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Welcome message')
+    embed.add_field(name = '__Welcome to Our Server__',value ='**Thanks for Joining our Server Hope you enjoy please respect all members and staff.**',inline = False)
+    embed.set_image(url = 'https://cdn.discordapp.com/attachments/520159870448566287/524894568072609816/youre_welcome_obama.gif')
+    await client.send_message(member,embed=embed)
+    print("Sent message to " + member.name)
+    channel = discord.utils.get(client.get_all_channels(), server__name='bysoyal2', name='â˜…å½¡-welcome-å½¡â˜…')
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='Do not forget to check Rules and never try to break any one of them', color = discord.Color((r << 16) + (g << 8) + b))
+    embed.add_field(name='__Thanks for joining__', value='**Hope you will be active here.**', inline=True)
+    embed.add_field(name='Your join position is', value=member.joined_at)
+    embed.set_image(url = 'https://cdn.discordapp.com/attachments/520159870448566287/524894568072609816/youre_welcome_obama.gif')
+    embed.set_thumbnail(url=member.avatar_url)
+    await client.send_message(channel, embed=embed)		
+
+	
+	
+@client.command(pass_context=True)
+async def virus(ctx,user: discord.Member=None,*,hack=None):
+    nome = ctx.message.author
+    if not hack:
+        hack = 'discord'
+    else:
+        hack = hack.replace(' ','_')
+    channel = ctx.message.channel
+    x = await client.send_message(channel, '``[â–“â–“â–“                    ] / {}-virus.exe Packing files.``'.format(hack))
+    await asyncio.sleep(1.5)
+    x = await client.edit_message(x,'``[â–“â–“â–“â–“â–“â–“â–“                ] - {}-virus.exe Packing files..``'.format(hack))
+    await asyncio.sleep(0.3)
+    x = await client.edit_message(x,'``[â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“           ] \ {}-virus.exe Packing files...``'.format(hack))
+    await asyncio.sleep(1.2)
+    x = await client.edit_message(x,'``[â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“         ] | {}-virus.exe Initializing code.``'.format(hack))
+    await asyncio.sleep(1)
+    x = await client.edit_message(x,'``[â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“      ] / {}-virus.exe Initializing code..``'.format(hack))
+    await asyncio.sleep(1.5)
+    x = await client.edit_message(x,'``[â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“   ] - {}-virus.exe Finishing.``'.format(hack))
+    await asyncio.sleep(1)
+    x = await client.edit_message(x,'``[â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“â–“ ] \ {}-virus.exe Finishing..``'.format(hack))
+    await asyncio.sleep(1)
+    x = await client.edit_message(x,'``Successfully downloaded {}-virus.exe``'.format(hack))
+    await asyncio.sleep(2)
+    x = await client.edit_message(x,'``Injecting virus.   |``')
+    await asyncio.sleep(0.5)
+    x = await client.edit_message(x,'``Injecting virus..  /``')
+    await asyncio.sleep(0.5)
+    x = await client.edit_message(x,'``Injecting virus... -``')
+    await asyncio.sleep(0.5)
+    x = await client.edit_message(x,'``Injecting virus....\``')
+    await client.delete_message(x)
+    await client.delete_message(ctx.message)
+        
+    if user:
+        await client.say('`{}-virus.exe` successfully injected into **{}**\'s system.'.format(hack,user.name))
+        await client.send_message(user,'**Alert!**\n``You may have been hacked. {}-virus.exe has been found in your system\'s operating system.\nYour data may have been compromised. Please re-install your OS immediately.``'.format(hack))
+    else:
+        await client.say('**{}** has hacked himself Â¯\_(ãƒ„)_/Â¯.'.format(name.name))
+        await client.send_message(name,'**Alert!**\n``You may have been hacked. {}-virus.exe has been found in your system\'s operating system.\nYour data may have been compromised. Please re-install your OS immediately.``'.format(hack))
+	
+
+@client.command(pass_context = True)
+async def meme(ctx):
+    choices = ['https://img.memecdn.com/english_o_869587.webp', 'https://img.memecdn.com/everybody-knows-muricans-don-amp-039-t-speak-english-the-same-way-mexicans-don-amp-039-t-speak-spanish_c_7233205.webp', 'https://img.memecdn.com/english-reaction-when-they-heard-about-eu_c_6994013.webp', 'https://images3.memedroid.com/images/UPLOADED393/5b0c3ee92799f.jpeg' , 'https://images7.memedroid.com/images/UPLOADED850/5b0c2d7dd6049.jpeg', 'https://images7.memedroid.com/images/UPLOADED905/5b0c30c468fa8.jpeg', 'https://images7.memedroid.com/images/UPLOADED726/5b0c2d4c5f288.jpeg', 'https://images7.memedroid.com/images/UPLOADED936/5b0c2a90adbe7.jpeg', 'https://images7.memedroid.com/images/UPLOADED764/5b0c1e491c669.jpeg', 'https://images3.memedroid.com/images/UPLOADED922/5b0c284b71cd0.jpeg', 'https://images3.memedroid.com/images/UPLOADED207/5b0c265a58cf4.jpeg', 'https://images7.memedroid.com/images/UPLOADED920/5b0c06813741a.jpeg', 'https://images3.memedroid.com/images/UPLOADED46/5a91c871e61f1.jpeg', 'https://images7.memedroid.com/images/UPLOADED737/5a91c7f234bd2.jpeg', 'https://images7.memedroid.com/images/UPLOADED757/5a91bd39e1323.jpeg', 'https://images7.memedroid.com/images/UPLOADED963/5a91b4f7aba7e.jpeg', 'https://images7.memedroid.com/images/UPLOADED794/5a91ac0900506.jpeg', 'https://images3.memedroid.com/images/UPLOADED188/5a91aa326ad4e.jpeg', 'https://www.google.co.in/search?q=meme&rlz=1C1CHBF_enIN808IN808&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjR59bhhZPfAhUZEnIKHRzUAvkQ_AUIDigB&biw=1366&bih=664']
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(title='Meme', description='<:upside_down:>', color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/520159870448566287/520829749095038977/pubg.png') 
+    embed.set_image(url = random.choice(choices))
+    await client.send_typing(ctx.message.channel)
+    await client.send_message(ctx.message.channel, embed=embed) 
+	
+	
+	
+	
+		
+client.run(os.getenv('Token'))
